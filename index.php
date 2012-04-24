37ebd7d7881bed336853720573b2d186bb6b12e7
Johan Steen <artstorm@gmail.com>
2012-04-24 20:46:09 +0200

    First commit with GH pages.

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+<meta charset="UTF-8" />
+<title>JS Turntable Viewer</title>
+<link rel="stylesheet" href="/assets/css/turntable.css" />
+<script type='text/javascript' src='/assets/js/swfobject.js?ver=2.2'></script>
+<script type="text/javascript">swfobject.embedSWF("/turntable/js-turntable.swf", "turntable", "640", "436", "9.0.115", "",{file : "http://turntableviewer.sourceforge.net/turntable/content/js-tt-gallery.xml", preview : "http://turntableviewer.sourceforge.net/turntable/content/js-tt-preview.jpg"},{"bgcolor" : "#333333"},{});</script>
+<meta name="description" content="The JS Turntable Viewer is a flash based application, suitable to embed rendered image sequences with turntables of 3D models or 3D sculpts on web pages. The Viewer can handle both a single turntable or a gallery of turntables." />
+<link rel="shortcut icon" href="http://turntableviewer.sourceforge.net/favicon.ico" type="image/x-icon" />
+</head>
+<body>
+<div id="header">
+	<div id="logo" class="container_16">
+		<h1 class="grid_16">JS Turntable Viewer</h1>
+	</div><!-- #logo -->
+</div><!-- #header -->
+<div class="clear"></div>
+
+<div id="wrapper">
+	<div id="content" class="container_16">
+
+		<div id="project-info" class="grid_5">
+			<h2>You spin me right round, baby!</h2>
+			<p>The JS Turntable Viewer is a flash based application, suitable to embed rendered image sequences with turntables of 3D models or 3D sculpts on web pages.
+			The Viewer can handle both a single turntable or a gallery of turntables.</p>
+			<p>Try the demo &raquo;&raquo;&raquo;</p>
+		</div><!-- Project Info -->
+		<div id="turntable-demo" class="grid_11">
+			<div id="turntable">
+				<p><small>Turntable Viewer would be here. No or old flash player installed.</small></p>
+				<p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://media.artstorm.net/flash/get_flash_player.gif" alt="Get Adobe Flash player" /></a></p>
+			</div><!-- #turntable -->
+		</div><!-- #turntable-demo -->
+		<div class="clear"></div>
+
+		<div id="developers" class="grid_6">
+			<h2>Developers</h2>
+			<h3>Join this project</h3>
+			<p>To join this project, please contact the project administrators of this project, as shown on the
+			<a href="http://sourceforge.net/projects/turntableviewer/">project summary page</a>.</p>
+									
+			<h3>Get the source code</h3>
+			<p>Source code for this project is available through the SCM repository used by the project, as accessible from the
+			<a href="http://sourceforge.net/projects/turntableviewer/develop">project develop page</a>.</p>
+		</div><!-- #developers -->
+
+		<div id="users" class="grid_6">
+			<h2>Users</h2>
+			<ul>
+				<li><a href="http://sourceforge.net/projects/turntableviewer/files">Download JS Turntable Viewer files</a></li>
+				<li><a href="http://sourceforge.net/project/project_donations.php?group_id=403382" class="donate" title="Donate to this project">Donate money</a></li>
+				<li><a href="http://sourceforge.net/projects/turntableviewer/">Project detail and discuss</a></li>
+				<li><a href="http://sourceforge.net/projects/turntableviewer/support">Get support</a></li>
+			</ul>
+		</div><!-- #users -->
+	
+		<div id="additional" class="grid_4">
+			<h2>Additional</h2>
+			<p>The project was originally hosted on my <a href="http://www.artstorm.net/" title="CG Portfolio">3D Portfolio</a> site and you can still find additional information about the viewer there.<br/>
+			<a href="http://www.artstorm.net/plugins/turntable-flash-viewer/">Visit my Turntable Viewer page &raquo;</a></p>
+		
+			<p>
+			<a href="http://sourceforge.net/projects/turntableviewer"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=403382&amp;type=11" width="120" height="30" alt="Get JS Turntable Viewer at SourceForge.net. Fast, secure and Free Open Source software downloads" /></a>			
+			<a href="http://sourceforge.net/donate/index.php?group_id=403382"><img src="http://images.sourceforge.net/images/project-support.jpg" width="88" height="32" alt="Support This Project" /></a>
+			</p>
+		</div><!-- #additional -->
+		<div class="clear"></div>
+	</div><!-- #content -->
+	<div class="clear"></div>
+</div><!-- #wrapper -->
+
+<div id="footer">
+    <div id="copyright">
+		Copyright Johan Steen <? echo date('Y'); ?>.
+    </div><!-- #copyright -->
+</div><!-- #footer -->
+</body>
+</html>
